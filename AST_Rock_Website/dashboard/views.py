b1d8b5f121ae69230a42820f78d74d87a80042d7
utzerath <jackutzerath@icloud.com>
2024-06-19 21:38:34 -0600

    minor changes

--- AST_Rock_Website/dashboard/views.py
+++ AST_Rock_Website/dashboard/views.py
@@ -8,8 +8,6 @@ from allauth.socialaccount.providers.oauth2.views import OAuth2Adapter
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.views import SignupView
 from allauth.socialaccount.models import SocialAccount
-
-
 from django.contrib.auth.decorators import login_required
 from allauth.socialaccount.models import SocialAccount, SocialToken
 import requests
@@ -17,28 +15,40 @@ import requests
 
 @login_required
 def your_repositories(request):
-    user = request.user  # It's better to use 'user' instead of 'username' for clarity
+    user = request.user
     print(user)
+    
     try:
-        print("right here")
         # Get the social token for GitHub
         social_account = SocialAccount.objects.get(user=user, provider='github')
-        print(social_account)
-        token = SocialToken.objects.get(account=social_account)
-        print(token)
+        token = SocialToken.objects.get(account=social_account).token
+        print(f"Token: {token}")
+
         # GitHub API endpoint to fetch repositories
         url = 'https://api.github.com/user/repos'
-        headers = {'Authorization': f'token {token.token}'}
+        headers = {'Authorization': f'token {token}'}
         response = requests.get(url, headers=headers)
-        repositories = response.json()  # This is a list of repositories
+        repositories = response.json()
+        
         repo_names = [repo['name'] for repo in repositories]
-        print(repo_names)
+        print(f"Repositories: {repo_names}")
+
+        # Store data in the session
+        request.session['github_token'] = token
+        request.session['username'] = user.username
+        request.session['repositories'] = repo_names
+
         # Send both username and repo_names to the template
-        return render(request, 'your_repositories.html', {'username': user.username, 'repositories': repo_names})
+        return render(request, 'your_repositories.html', {
+            'username': user.username,
+            'repositories': repo_names
+        })
+        
     except SocialToken.DoesNotExist:
-        print("GitHub token failed")
+        print("GitHub token not found")
         return render(request, 'your_repositories.html', {'error': 'GitHub token not found'})
 
+@login_required
 def repo_detail(request, repo_name):
     
     return render(request, 'repo_detail.html', {'repo_name': repo_name})
