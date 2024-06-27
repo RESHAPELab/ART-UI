dbe5235852c7f20a5d084c60d546e340318a0f9f
utzerath <jackutzerath@icloud.com>
2024-06-27 11:25:06 -0600

    GPT integration

--- AST_Rock_Website/dashboard/views.py
+++ AST_Rock_Website/dashboard/views.py
@@ -127,13 +127,14 @@ def repo_detail(request, repo_name):
         try:
             response = external.predict_issue(issue)
             responses_rf.append(response)
+            response = external2.predict_issue(issue)
             responses_gpt.append(response)
         except AttributeError as e:
             print(f"Error processing issue {issue.number}: {str(e)}")
     print(responses_rf)
     print(responses_gpt)
     # Zip lists together for easier template use
-    issues_responses = zip(issues, responses_rf)
+    issues_responses = zip(issues, responses_rf, responses_gpt)
     
     return render(request, 'repo_detail.html', {
         'repo_name': repo_name,
