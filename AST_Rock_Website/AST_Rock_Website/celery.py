a23044dc5bcd0a40b0314f7b54296696c2d8fca6
utzerath <jackutzerath@icloud.com>
2024-07-02 19:27:32 -0600

    Config2

--- AST_Rock_Website/AST_Rock_Website/celery.py
+++ AST_Rock_Website/AST_Rock_Website/celery.py
@@ -12,9 +12,7 @@ app = Celery('AST_Rock_Website')
 # Use REDIS_TLS_URL for secure connection to Redis
 app.conf.broker_url = 'redis://:p155c9e6f4e3b35d7dc1cea4cc35049b9eb52e6d961b10d04485088c365c3fe84@ec2-54-166-204-188.compute-1.amazonaws.com:17489'
 
-app.conf.broker_use_ssl = {
-    'ssl_cert_reqs': ssl.CERT_NONE
-}
+
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
