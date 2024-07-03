b14eb6fe1f8bb0045ecb9de44c4efcfb784ce1a5
utzerath <jackutzerath@icloud.com>
2024-07-02 18:38:56 -0600

    add django celery settings

--- AST_Rock_Website/AST_Rock_Website/celery.py
+++ AST_Rock_Website/AST_Rock_Website/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AST_Rock_Website.settings')
 
 app = Celery('AST_Rock_Website')
 
--- AST_Rock_Website/AST_Rock_Website/settings.py
+++ AST_Rock_Website/AST_Rock_Website/settings.py
@@ -30,8 +30,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['still-mesa-24591-bad3365c7700.herokuapp.com', 'localhost', '127.0.0.1']
 
-CELERY_BROKER_URL = os.getenv('REDIS_TLS_URL')
-
+CELERY_BROKER_URL = os.getenv('REDIS_TLS_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_TLS_URL', 'redis://localhost:6379/0')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
 # Application definition
 
 INSTALLED_APPS = [
